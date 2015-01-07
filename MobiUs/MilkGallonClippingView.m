d0e2da2e7e504bafc794180454f41119dee5a4d4
MylesCaley <myles.caley@gmail.com>
2015-01-07 12:36:35 -0500

    milk jug percent now working with clipping mask

--- MobiUs/MilkGallonClippingView.m
+++ MobiUs/MilkGallonClippingView.m
@@ -16,79 +16,73 @@
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     [super drawRect:rect];
-    UIColor* strokeColor = [UIColor colorWithRed: 0 green: 0 blue: 0 alpha: 1];
+    UIColor* strokeColor = UIColorFromRGB(0x717073);
+    UIColor* strokeColor1 = UIColorFromRGB(0x717073);
+    UIColor* strokeColor2 = UIColorFromRGB(0x717073);
     UIBezierPath* path = [UIBezierPath bezierPath];
-    [path moveToPoint: CGPointMake(1935,1448)];
-    [path addCurveToPoint: CGPointMake(1905,1232) controlPoint1: CGPointMake(1921,1316) controlPoint2: CGPointMake(1928,1266)];
-    [path addCurveToPoint: CGPointMake(1778,1151) controlPoint1: CGPointMake(1883,1198) controlPoint2: CGPointMake(1790,1167)];
-    [path addCurveToPoint: CGPointMake(1767,1136) controlPoint1: CGPointMake(1771,1141) controlPoint2: CGPointMake(1767,1136)];
-    [path addCurveToPoint: CGPointMake(1767,1134) controlPoint1: CGPointMake(1767,1136) controlPoint2: CGPointMake(1767,1135)];
-    [path addCurveToPoint: CGPointMake(1769,1129) controlPoint1: CGPointMake(1768,1133) controlPoint2: CGPointMake(1769,1131)];
-    [path addCurveToPoint: CGPointMake(1765,1124) controlPoint1: CGPointMake(1769,1127) controlPoint2: CGPointMake(1767,1125)];
-    [path addCurveToPoint: CGPointMake(1764,1117) controlPoint1: CGPointMake(1765,1120) controlPoint2: CGPointMake(1764,1117)];
-    [path addCurveToPoint: CGPointMake(1764,1083) controlPoint1: CGPointMake(1764,1115) controlPoint2: CGPointMake(1764,1083)];
-    [path addCurveToPoint: CGPointMake(1711,1075) controlPoint1: CGPointMake(1764,1083) controlPoint2: CGPointMake(1762,1075)];
-    [path addCurveToPoint: CGPointMake(1661,1086) controlPoint1: CGPointMake(1711,1075) controlPoint2: CGPointMake(1661,1075)];
-    [path addCurveToPoint: CGPointMake(1661,1118) controlPoint1: CGPointMake(1661,1098) controlPoint2: CGPointMake(1661,1118)];
-    [path addLineToPoint: CGPointMake(1659,1123)];
-    [path addCurveToPoint: CGPointMake(1654,1129) controlPoint1: CGPointMake(1656,1123) controlPoint2: CGPointMake(1654,1126)];
-    [path addCurveToPoint: CGPointMake(1656,1134) controlPoint1: CGPointMake(1654,1131) controlPoint2: CGPointMake(1654,1133)];
-    [path addLineToPoint: CGPointMake(1653,1143)];
-    [path addLineToPoint: CGPointMake(1579,1189)];
-    [path addLineToPoint: CGPointMake(1569,1226)];
-    [path addLineToPoint: CGPointMake(1544,1253)];
-    [path addCurveToPoint: CGPointMake(1496,1604) controlPoint1: CGPointMake(1544,1253) controlPoint2: CGPointMake(1496,1461)];
-    [path addCurveToPoint: CGPointMake(1503,1706) controlPoint1: CGPointMake(1496,1672) controlPoint2: CGPointMake(1503,1706)];
-    [path addCurveToPoint: CGPointMake(1527,1745) controlPoint1: CGPointMake(1503,1706) controlPoint2: CGPointMake(1512,1731)];
-    [path addCurveToPoint: CGPointMake(1591,1759) controlPoint1: CGPointMake(1541,1759) controlPoint2: CGPointMake(1566,1759)];
-    [path addCurveToPoint: CGPointMake(1821,1758) controlPoint1: CGPointMake(1616,1759) controlPoint2: CGPointMake(1821,1758)];
-    [path addLineToPoint: CGPointMake(1832,1749)];
-    [path addLineToPoint: CGPointMake(1843,1760)];
-    [path addLineToPoint: CGPointMake(1891,1760)];
-    [path addCurveToPoint: CGPointMake(1926,1725) controlPoint1: CGPointMake(1891,1760) controlPoint2: CGPointMake(1917,1761)];
-    [path addCurveToPoint: CGPointMake(1935,1448) controlPoint1: CGPointMake(1935,1690) controlPoint2: CGPointMake(1950,1579)];
+    UIBezierPath* path1 = [UIBezierPath bezierPath];
+    UIBezierPath* path2 = [UIBezierPath bezierPath];
+    path.lineWidth = 7.88;
+    path1.lineWidth = 7.88;
+    path2.lineWidth = 7.88;
+    self.clippingPath = path;
+
+    [path moveToPoint: CGPointMake(207,176)];
+    [path addCurveToPoint: CGPointMake(193,76) controlPoint1: CGPointMake(200,115) controlPoint2: CGPointMake(203,92)];
+    [path addCurveToPoint: CGPointMake(133,38) controlPoint1: CGPointMake(182,60) controlPoint2: CGPointMake(139,46)];
+    [path addCurveToPoint: CGPointMake(129,31) controlPoint1: CGPointMake(130,34) controlPoint2: CGPointMake(129,31)];
+    [path addCurveToPoint: CGPointMake(129,30) controlPoint1: CGPointMake(129,31) controlPoint2: CGPointMake(129,31)];
+    [path addCurveToPoint: CGPointMake(129,28) controlPoint1: CGPointMake(129,30) controlPoint2: CGPointMake(129,29)];
+    [path addCurveToPoint: CGPointMake(128,25) controlPoint1: CGPointMake(129,27) controlPoint2: CGPointMake(129,26)];
+    [path addCurveToPoint: CGPointMake(127,22) controlPoint1: CGPointMake(127,24) controlPoint2: CGPointMake(127,23)];
+    [path addCurveToPoint: CGPointMake(127,7) controlPoint1: CGPointMake(127,21) controlPoint2: CGPointMake(127,7)];
+    [path addCurveToPoint: CGPointMake(102,3) controlPoint1: CGPointMake(127,7) controlPoint2: CGPointMake(126,3)];
+    [path addCurveToPoint: CGPointMake(79,8) controlPoint1: CGPointMake(102,3) controlPoint2: CGPointMake(79,3)];
+    [path addCurveToPoint: CGPointMake(79,23) controlPoint1: CGPointMake(79,14) controlPoint2: CGPointMake(79,23)];
+    [path addLineToPoint: CGPointMake(78,25)];
+    [path addCurveToPoint: CGPointMake(76,28) controlPoint1: CGPointMake(77,25) controlPoint2: CGPointMake(76,27)];
+    [path addCurveToPoint: CGPointMake(77,30) controlPoint1: CGPointMake(76,29) controlPoint2: CGPointMake(76,30)];
+    [path addLineToPoint: CGPointMake(75,34)];
+    [path addLineToPoint: CGPointMake(41,56)];
+    [path addLineToPoint: CGPointMake(36,73)];
+    [path addLineToPoint: CGPointMake(25,86)];
+    [path addCurveToPoint: CGPointMake(3,249) controlPoint1: CGPointMake(25,86) controlPoint2: CGPointMake(3,183)];
+    [path addCurveToPoint: CGPointMake(6,296) controlPoint1: CGPointMake(3,281) controlPoint2: CGPointMake(6,296)];
+    [path addCurveToPoint: CGPointMake(17,315) controlPoint1: CGPointMake(6,296) controlPoint2: CGPointMake(10,308)];
+    [path addCurveToPoint: CGPointMake(47,322) controlPoint1: CGPointMake(23,322) controlPoint2: CGPointMake(35,322)];
+    [path addCurveToPoint: CGPointMake(154,321) controlPoint1: CGPointMake(58,322) controlPoint2: CGPointMake(154,321)];
+    [path addLineToPoint: CGPointMake(159,317)];
+    [path addLineToPoint: CGPointMake(164,322)];
+    [path addLineToPoint: CGPointMake(186,322)];
+    [path addCurveToPoint: CGPointMake(203,306) controlPoint1: CGPointMake(186,322) controlPoint2: CGPointMake(198,322)];
+    [path addCurveToPoint: CGPointMake(207,176) controlPoint1: CGPointMake(207,289) controlPoint2: CGPointMake(214,238)];
     [strokeColor setStroke];
-    path.lineWidth = 0.5;
+    //path.lineWidth = 7.88;
     [path stroke];
     
-    self.clippingPath = path;
-    
-    UIColor* strokeColor1 = [UIColor colorWithRed: 0 green: 0 blue: 0 alpha: 1];
-    UIBezierPath* path1 = [UIBezierPath bezierPath];
-    [path1 moveToPoint: CGPointMake(1660,1120)];
-    [path1 addCurveToPoint: CGPointMake(1713,1114) controlPoint1: CGPointMake(1675,1116) controlPoint2: CGPointMake(1693,1114)];
-    [path1 addCurveToPoint: CGPointMake(1765,1120) controlPoint1: CGPointMake(1732,1114) controlPoint2: CGPointMake(1750,1116)];
+    //UIColor* strokeColor1 = [UIColor colorWithRed: 0 green: 0 blue: 0 alpha: 1];
+    //UIBezierPath* path1 = [UIBezierPath bezierPath];
+    [path1 moveToPoint: CGPointMake(70,158)];
+    [path1 addCurveToPoint: CGPointMake(60,167) controlPoint1: CGPointMake(70,163) controlPoint2: CGPointMake(65,167)];
+    [path1 addLineToPoint: CGPointMake(60,167)];
+    [path1 addCurveToPoint: CGPointMake(51,157) controlPoint1: CGPointMake(55,166) controlPoint2: CGPointMake(51,162)];
+    [path1 addLineToPoint: CGPointMake(57,76)];
+    [path1 addCurveToPoint: CGPointMake(67,68) controlPoint1: CGPointMake(57,71) controlPoint2: CGPointMake(62,67)];
+    [path1 addLineToPoint: CGPointMake(67,68)];
+    [path1 addCurveToPoint: CGPointMake(75,78) controlPoint1: CGPointMake(72,68) controlPoint2: CGPointMake(76,72)];
+    [path1 addLineToPoint: CGPointMake(70,158)];
     [strokeColor1 setStroke];
-    path1.lineWidth = 0.5;
+    //path1.lineWidth = 0.5;
     [path1 stroke];
     
-    UIColor* strokeColor2 = [UIColor colorWithRed: 0 green: 0 blue: 0 alpha: 1];
-    UIBezierPath* path2 = [UIBezierPath bezierPath];
-    [path2 moveToPoint: CGPointMake(1642,1408)];
-    [path2 addCurveToPoint: CGPointMake(1620,1427) controlPoint1: CGPointMake(1641,1419) controlPoint2: CGPointMake(1631,1427)];
-    [path2 addLineToPoint: CGPointMake(1620,1427)];
-    [path2 addCurveToPoint: CGPointMake(1601,1405) controlPoint1: CGPointMake(1609,1426) controlPoint2: CGPointMake(1601,1416)];
-    [path2 addLineToPoint: CGPointMake(1613,1233)];
-    [path2 addCurveToPoint: CGPointMake(1634,1214) controlPoint1: CGPointMake(1613,1222) controlPoint2: CGPointMake(1623,1213)];
-    [path2 addLineToPoint: CGPointMake(1634,1214)];
-    [path2 addCurveToPoint: CGPointMake(1653,1236) controlPoint1: CGPointMake(1645,1215) controlPoint2: CGPointMake(1654,1225)];
-    [path2 addLineToPoint: CGPointMake(1642,1408)];
+    //UIColor* strokeColor2 = [UIColor colorWithRed: 0 green: 0 blue: 0 alpha: 1];
+    //UIBezierPath* path2 = [UIBezierPath bezierPath];
+    [path2 moveToPoint: CGPointMake(37,71)];
+    [path2 addCurveToPoint: CGPointMake(24,166) controlPoint1: CGPointMake(37,71) controlPoint2: CGPointMake(28,159)];
+    [path2 addCurveToPoint: CGPointMake(14,177) controlPoint1: CGPointMake(20,172) controlPoint2: CGPointMake(14,177)];
     [strokeColor2 setStroke];
-    path2.lineWidth = 0.5;
+    //path2.lineWidth = 0.5;
     [path2 stroke];
-    
-    UIColor* strokeColor3 = [UIColor colorWithRed: 0 green: 0 blue: 0 alpha: 1];
-    UIBezierPath* path3 = [UIBezierPath bezierPath];
-    [path3 moveToPoint: CGPointMake(1570,1222)];
-    [path3 addCurveToPoint: CGPointMake(1542,1425) controlPoint1: CGPointMake(1570,1222) controlPoint2: CGPointMake(1552,1411)];
-    [path3 addCurveToPoint: CGPointMake(1520,1450) controlPoint1: CGPointMake(1533,1439) controlPoint2: CGPointMake(1520,1450)];
-    [strokeColor3 setStroke];
-    path3.lineWidth = 0.5;
-    [path3 stroke];
-    
-    
-
-    
     
 }
 
--- MobiUs/ScaleViewController.h
+++ MobiUs/ScaleViewController.h
@@ -16,6 +16,7 @@
 @property (strong, nonatomic) FSTMilkyWeigh *milkyWeigh;
 @property (strong, nonatomic) IBOutlet UIImageView *milkJugView;
 @property (strong, nonatomic) IBOutlet UIImageView *backgroundView;
-@property (strong, nonatomic) IBOutlet UIView *whiteView;
+@property (strong, nonatomic) IBOutlet UIView *whiteView; //view that lays in the white behind the jug
+@property (strong, nonatomic) IBOutlet MilkGallonClippingView *clippingView; //view that draws and clips the white view
 
 @end

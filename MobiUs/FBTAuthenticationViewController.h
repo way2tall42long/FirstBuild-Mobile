6f5fed5d379db15097318f386b4673e1f9b3441f
MylesCaley <myles.caley@gmail.com>
2015-01-22 14:31:26 -0500

    - cleaning up authentication controllers to use only Facebook and Google+

--- MobiUs/FBTAuthenticationViewController.h
+++ MobiUs/FBTAuthenticationViewController.h
@@ -15,20 +15,10 @@
 
 @interface FBTAuthenticationViewController : UIViewController <GPPSignInDelegate,FBLoginViewDelegate>
 
-@property (strong, nonatomic) IBOutlet UITextField *usernameTextField;
-@property (strong, nonatomic) IBOutlet UITextField *passwordTextField;
-@property (strong, nonatomic) IBOutlet UIButton *signInButton;
-@property (strong, nonatomic) IBOutlet UIButton *registerButton;
 @property (strong, nonatomic) IBOutlet UIActivityIndicatorView *loginActivityIndicator;
 @property (strong, nonatomic) MPMoviePlayerController *backgroundMovie;
 @property (strong, nonatomic) IBOutlet UIControl *controlView;
 @property (strong, nonatomic) IBOutlet GPPSignInButton *googleLoginView;
 @property (strong, nonatomic) IBOutlet FBLoginView *facebookLoginView;
-
-- (IBAction)signInButtonPressed:(id)sender;
-- (IBAction)registerButtonPressed:(id)sender;
-- (IBAction)backgroundTap:(id)sender;
-- (IBAction)usernameDoneEditing:(id)sender;
-- (IBAction)passwordDoneEditing:(id)sender;
 
 @end
--- MobiUs/FBTAuthenticationViewController.m
+++ MobiUs/FBTAuthenticationViewController.m
@@ -14,16 +14,27 @@
 #import <GoogleOpenSource/GoogleOpenSource.h>
 #import <FacebookSDK/FacebookSDK.h>
 
-
 @interface FBTAuthenticationViewController ()
 
 @end
 
 @implementation FBTAuthenticationViewController
 
+#pragma mark - GPPSignInDelegate
+
+- (void)initGoogleAuth
+{
+    GPPSignIn *signIn = [GPPSignIn sharedInstance];
+    signIn.shouldFetchGoogleUserEmail = YES;
+    signIn.clientID = @"523108828656-nuardro4852mn2g66a8mg9ukj2ld4tf6.apps.googleusercontent.com";;
+    signIn.scopes = @[ @"profile" ];
+    signIn.delegate = self;
+    [signIn trySilentAuthentication];
+}
+
 - (void)finishedWithAuth:(GTMOAuth2Authentication *)auth error:(NSError *)error
 {
-     Firebase *authRef = [[FirebaseShared sharedInstance] firebaseRootReference];
+    Firebase *authRef = [[FirebaseShared sharedInstance] firebaseRootReference];
     FBTUser *user = [FBTUser sharedInstance];
     NSLog(@"Received error %@ and auth object %@",error, auth);
     [authRef authWithOAuthProvider:@"google" token:auth.accessToken
@@ -48,62 +59,22 @@
            }];
 }
 
+#pragma mark FBLoginViewDelegate
+
 -(void)loginViewFetchedUserInfo:(FBLoginView *)loginView user:(id<FBGraphUser>)user
 {
     NSLog(@"ok");
 }
 
+
+#pragma mark UIViewController
+
 - (void)viewDidLoad
 {
-    
-    //TODO: fix this... mostly temporary
     [super viewDidLoad];
     self.facebookLoginView.delegate = self;
-    //[self initGoogle];
-    
-        GPPSignIn *signIn = [GPPSignIn sharedInstance];
-        //signIn.shouldFetchGooglePlusUser = YES;
-        signIn.shouldFetchGoogleUserEmail = YES;  // Uncomment to get the user's email
-    
-        // You previously set kClientId in the "Initialize the Google+ client" step
-        signIn.clientID = @"523108828656-nuardro4852mn2g66a8mg9ukj2ld4tf6.apps.googleusercontent.com";;
-    
-        // Uncomment one of these two statements for the scope you chose in the previous step
-        //signIn.scopes = @[ kGTLAuthScopePlusLogin ];  // "https://www.googleapis.com/auth/plus.login" scope
-       signIn.scopes = @[ @"profile" ];            // "profile" scope
-        //signIn.scopes = @[];
-        // Optional: declare signIn.actions, see "app activities"
-        signIn.delegate = self;
-    [signIn trySilentAuthentication];
-    NSArray *accounts;
-    
-    [SSKeychain setAccessibilityType:kSecAttrAccessibleWhenUnlocked];
-    accounts = [SSKeychain accountsForService:@"firebase"];
-    
-    if (accounts)
-    {
-        NSString* account = [accounts[0] objectForKey:@"acct"];
-        NSString* password = [SSKeychain passwordForService:@"firebase" account:account];
-       
-        //if we have an account already stored in the keychain but this is the first launch
-        //then lets erase all
-        if (![[NSUserDefaults standardUserDefaults] objectForKey:@"isFirstLaunch"])
-        {
-            [accounts enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-                [SSKeychain deletePasswordForService:@"firebase" account:obj];
-            }];
-            
-            [[NSUserDefaults standardUserDefaults] setValue:@"true" forKey:@"isFirstLaunch"];
-            [[NSUserDefaults standardUserDefaults] synchronize];
-        }
-        else
-        {
-            self.usernameTextField.text = @"hi@hi.com";
-            self.passwordTextField.text = @"hi";
-        }
-    }
-
-    //TODO: don't play the movie if we are auto logging in
+    [self initGoogleAuth];
+   
     self.backgroundMovie = [[MPMoviePlayerController alloc] initWithContentURL:
                             [NSURL fileURLWithPath: [[NSBundle mainBundle]
                                                      pathForResource:@"splashvideo" ofType:@"mp4"]]];
@@ -121,7 +92,6 @@
     if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_6_1) {
         [translucentUnderlayment setAlpha:0.9];
     }
-    //translucentUnderlayment.layer.cornerRadius = 5;
     translucentUnderlayment.layer.masksToBounds = YES;
     [self.controlView addSubview:translucentUnderlayment];
     [self.controlView sendSubviewToBack:translucentUnderlayment];
@@ -133,161 +103,101 @@
     [super didReceiveMemoryWarning];
 }
 
-- (IBAction)signInButtonPressed:(id)sender;
-{
-    Firebase *authRef = [[FirebaseShared sharedInstance] firebaseRootReference];
-    FBTUser *user = [FBTUser sharedInstance];    // Open a session showing the user the login UI
-    [FBSession openActiveSessionWithReadPermissions:@[@"public_profile"] allowLoginUI:YES
-                                  completionHandler:^(FBSession *session, FBSessionState state, NSError *error) {
-                                      if (error) {
-                                          NSLog(@"Facebook login failed. Error: %@", error);
-                                      } else if (state == FBSessionStateOpen) {
-                                          NSString *accessToken = session.accessTokenData.accessToken;
-                                          [authRef authWithOAuthProvider:@"facebook" token:accessToken
-                                                      withCompletionBlock:^(NSError *error, FAuthData *authData) {
-                                                          if (error) {
-                                                              // Error authenticating with Firebase with OAuth token
-                                                          } else {
-                                                              // User is now logged in!
-                                                              NSLog(@"Successfully logged in! %@", authData);
-                                                              user.rootContainer = [@"/users/" stringByAppendingString:authData.uid];
-                                                              [[FirebaseShared sharedInstance] setUserBaseReference:[authRef childByAppendingPath:user.rootContainer]];
-                                                              
-                                                              Firebase *userConnectedRef = [[[FirebaseShared sharedInstance] userBaseReference] childByAppendingPath:@"connected"];
-                                                              [userConnectedRef setValue:@YES];
-                                                              [userConnectedRef onDisconnectRemoveValue];
-                                                              
-                                                              //[SSKeychain setPassword:password forService:@"firebase" account:username];
-                                                              [self.presentingViewController dismissViewControllerAnimated:NO completion:nil];
-                                                              [self performSegueWithIdentifier:@"main" sender:self];
-                                                              
-                                                          }
 
-                                                      }];
-                                      }
-                                  }];
-//    GPPSignIn *signIn = [GPPSignIn sharedInstance];
-//    //signIn.shouldFetchGooglePlusUser = YES;
-//    signIn.shouldFetchGoogleUserEmail = YES;  // Uncomment to get the user's email
-//    
-//    // You previously set kClientId in the "Initialize the Google+ client" step
-//    signIn.clientID = @"523108828656-nuardro4852mn2g66a8mg9ukj2ld4tf6.apps.googleusercontent.com";;
-//    
-//    // Uncomment one of these two statements for the scope you chose in the previous step
-//    //signIn.scopes = @[ kGTLAuthScopePlusLogin ];  // "https://www.googleapis.com/auth/plus.login" scope
-//   signIn.scopes = @[ @"profile" ];            // "profile" scope
-//    //signIn.scopes = @[];
-//    // Optional: declare signIn.actions, see "app activities"
-//    signIn.delegate = self;
-//    [signIn authenticate];
-//    GPPSignIn *signIn = [GPPSignIn sharedInstance];
-//    
+#pragma mark FirebaseSimpleLogin
+
+//TODO: Firebase Simple Login authentication is currently disabled. e-mail is not validated and requires
+//a secondary server to send verification e-mails. This code needs fixed as well.
+
+//- (void)initFirebaseAuthentication
+//{
+//    NSArray *accounts;
+//
+//    [SSKeychain setAccessibilityType:kSecAttrAccessibleWhenUnlocked];
+//    accounts = [SSKeychain accountsForService:@"firebase"];
+//
+//    if (accounts)
+//    {
+//        NSString* account = [accounts[0] objectForKey:@"acct"];
+//        NSString* password = [SSKeychain passwordForService:@"firebase" account:account];
+//
+//        //if we have an account already stored in the keychain but this is the first launch
+//        //then lets erase all
+//        if (![[NSUserDefaults standardUserDefaults] objectForKey:@"isFirstLaunch"])
+//        {
+//            [accounts enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+//                [SSKeychain deletePasswordForService:@"firebase" account:obj];
+//            }];
+//
+//            [[NSUserDefaults standardUserDefaults] setValue:@"true" forKey:@"isFirstLaunch"];
+//            [[NSUserDefaults standardUserDefaults] synchronize];
+//        }
+//        else
+//        {
+//            self.usernameTextField.text = @"hi@hi.com";
+//            self.passwordTextField.text = @"hi";
+//        }
+//    }
+//}
+
+//
+//- (IBAction)registerButtonPressed:(id)sender;
+//{
 //    [self.usernameTextField resignFirstResponder];
 //    [self.passwordTextField resignFirstResponder];
 //    [self.loginActivityIndicator startAnimating];
-//    [self loginWithUsernameAndLoadMainApp:self.usernameTextField.text havingPassword:self.passwordTextField.text];
-}
-
-
-- (IBAction)registerButtonPressed:(id)sender;
-{
-    [self.usernameTextField resignFirstResponder];
-    [self.passwordTextField resignFirstResponder];
-    [self.loginActivityIndicator startAnimating];
-    
-    Firebase *authRef = [[FirebaseShared sharedInstance] firebaseRootReference];
-    
-    [authRef createUser:self.usernameTextField.text  password:self.passwordTextField.text withCompletionBlock:^(NSError *error) {
-         if (error != nil) {
-             // There was an error creating the account
-             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Registration Failed!" message:@"Please check your email and password, and try again." delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
-             [alert show];
-             [self.loginActivityIndicator stopAnimating];
-         }
-         else
-         {
-             [self loginWithUsernameAndLoadMainApp:self.usernameTextField.text havingPassword:self.passwordTextField.text];
-         }
-    }];
-}
-
-- (void) loginWithUsernameAndLoadMainApp:(NSString*)username havingPassword:(NSString*)password
-{
-    
-    // TODO: use setValue with completion block for the creation of the new user
-    // TODO: better error checking
-    Firebase *authRef = [[FirebaseShared sharedInstance] firebaseRootReference];
-    FBTUser *user = [FBTUser sharedInstance];
-    
-    [authRef authUser:username  password:password withCompletionBlock:^(NSError *error,  FAuthData *authData) {
-        if (error == nil)
-        {
-            user.rootContainer = [@"/users/" stringByAppendingString:authData.uid];
-            [[FirebaseShared sharedInstance] setUserBaseReference:[authRef childByAppendingPath:user.rootContainer]];
-           
-            user.token = authData.token;
-            user.email = username;
-            
-            Firebase *userConnectedRef = [[[FirebaseShared sharedInstance] userBaseReference] childByAppendingPath:@"connected"];
-            [userConnectedRef setValue:@YES];
-            [userConnectedRef onDisconnectRemoveValue];
-            
-            [SSKeychain setPassword:password forService:@"firebase" account:username];
-            [self.presentingViewController dismissViewControllerAnimated:NO completion:nil];
-            [self performSegueWithIdentifier:@"main" sender:self];
-            
-        }
-        else
-        {
-            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Login Failed!" message:@"Please check your email and password, and try again." delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
-            [alert show];
-        }
-        [self.loginActivityIndicator stopAnimating];
-    }];
-
-}
-
-- (IBAction)backgroundTap:(id)sender;
-{
-    [self.usernameTextField resignFirstResponder];
-    [self.passwordTextField resignFirstResponder];
-}
-
-- (IBAction)usernameDoneEditing:(id)sender;
-{
-    [self.passwordTextField becomeFirstResponder];
-}
-
-- (IBAction)passwordDoneEditing:(id)sender;
-{
-    [self.passwordTextField resignFirstResponder];
-}
-
-# pragma mark - Keyboard will show/hide notifications to scroll the view properly
-//- (void)viewWillAppear:(BOOL)animated;
-//{
-//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
-//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
-//}
 //
-//- (void)viewWillDisappear:(BOOL)animated;
-//{
-//    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
-//    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
-//}
+//    Firebase *authRef = [[FirebaseShared sharedInstance] firebaseRootReference];
 //
-//- (void)keyboardWillShow:(NSNotification *)notification
-//{
-//    [UIView animateWithDuration:0.3f animations:^{
-//        [self.view setFrame:[[UIScreen mainScreen] bounds]];
+//    [authRef createUser:self.usernameTextField.text  password:self.passwordTextField.text withCompletionBlock:^(NSError *error) {
+//        if (error != nil) {
+//            // There was an error creating the account
+//            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Registration Failed!" message:@"Please check your email and password, and try again." delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
+//            [alert show];
+//            [self.loginActivityIndicator stopAnimating];
+//        }
+//        else
+//        {
+//            [self loginWithUsernameAndLoadMainApp:self.usernameTextField.text havingPassword:self.passwordTextField.text];
+//        }
 //    }];
 //}
-//
-//-(void)keyboardWillHide:(NSNotification *)notification
+
+//- (void) loginWithUsernameAndLoadMainApp:(NSString*)username havingPassword:(NSString*)password
 //{
-//    [UIView animateWithDuration:0.3f animations:^{
-//        [self.view setFrame:[[UIScreen mainScreen] bounds]];
+//    
+//    // TODO: use setValue with completion block for the creation of the new user
+//    // TODO: better error checking
+//    Firebase *authRef = [[FirebaseShared sharedInstance] firebaseRootReference];
+//    FBTUser *user = [FBTUser sharedInstance];
+//    
+//    [authRef authUser:username  password:password withCompletionBlock:^(NSError *error,  FAuthData *authData) {
+//        if (error == nil)
+//        {
+//            user.rootContainer = [@"/users/" stringByAppendingString:authData.uid];
+//            [[FirebaseShared sharedInstance] setUserBaseReference:[authRef childByAppendingPath:user.rootContainer]];
+//            
+//            user.token = authData.token;
+//            user.email = username;
+//            
+//            Firebase *userConnectedRef = [[[FirebaseShared sharedInstance] userBaseReference] childByAppendingPath:@"connected"];
+//            [userConnectedRef setValue:@YES];
+//            [userConnectedRef onDisconnectRemoveValue];
+//            
+//            [SSKeychain setPassword:password forService:@"firebase" account:username];
+//            [self.presentingViewController dismissViewControllerAnimated:NO completion:nil];
+//            [self performSegueWithIdentifier:@"main" sender:self];
+//            
+//        }
+//        else
+//        {
+//            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Login Failed!" message:@"Please check your email and password, and try again." delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
+//            [alert show];
+//        }
+//        [self.loginActivityIndicator stopAnimating];
 //    }];
+//    
 //}
+
 
 @end
